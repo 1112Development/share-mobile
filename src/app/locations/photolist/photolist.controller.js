46fc88931edac803f1760a3f77d94eb4975a3f47
ETinLV <ericturnernv@gmail.com>
2016-10-15 20:55:39 -0700

    get location on page load

--- src/app/locations/photolist/photolist.controller.js
+++ src/app/locations/photolist/photolist.controller.js
@@ -1,7 +1,18 @@
 class PhotolistController {
-  constructor() {
+  constructor($cordovaGeolocation) {
     'ngInject';
-
+    var posOptions = {timeout: 1000, enableHighAccuracy: false};
+    function getLocation() {
+      $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
+        window.localStorage.setItem('lat', position.coords.latitude);
+        window.localStorage.setItem('long', position.coords.longitude);
+      }, function(err) {
+        // error
+      })};
+    getLocation();
+    console.log('Where Am I?', window.localStorage.getItem('lat'), window.localStorage.getItem('long'));
   }
 }
 
