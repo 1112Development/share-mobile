f016cc54f7a498f738dae1d9c66923b23a2f2cb2
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-10-24 00:27:32 -0700

    add x to cancel image

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -15,32 +15,41 @@ class HomeController {
     this.list = [];
     this.modalState = false;
     this.URI = "";
+    this.photoExists = false;
 
     this.toggleModal = (view) => {
       this.modalState =  view === 'show' ? true : false;
     }
 
+    this.cancelImage = () => {
+      this.photoExists = false;
+      this.URI = "";
+    }
+
+    this.addImage = (source) => {
+        //hides modal
+        this.modalState = false;
+        
+        // Take The photo
+        this._Photos.newPhoto(source).then((imageURI) => {
+        
+        this.URI = this._Photos.toDataUrl(imageURI,function(base64img){
+            return base64img;
+        });
+
+        this.photoExists = true;
+
+        
+
+
+      }, function(err) {
+        console.err(err);
+      });
+    }
+
   }
 
-  addImage(source) {
-    //hides modal
-    this.modalState = false;
-    // Take The photo
-  this._Photos.newPhoto(source).then((imageURI) => {
-    console.log('wtf!!!')
-    this.URI = this._Photos.toDataUrl(imageURI,function(base64img){
-        return base64img;
-    });
 
-    // save to object ** In Progress
-
-   
-    // image.src = "data:image/jpeg;base64," + this.URI.src;
-
-  }, function(err) {
-    console.err(err);
-  });
-};
 
   
 
