7c077f63dbf5fb49c4120fd7e791b3dc0aed02c9
ETinLV <ericturnernv@gmail.com>
2016-10-25 15:25:27 -0700

    refactor

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -1,24 +1,21 @@
 class HomeController {
   constructor(images, $ionicPlatform, $http, Photos, AppConstants, Location, Mixpanel) {
     'ngInject';
-    // Aliases
+
     this.name = "HomeController";
     this._$ionicPlatform = $ionicPlatform;
     this._$http = $http;
     this._Location = Location;
     this._Photos = Photos;
 
-    // Var
-    this.list = [];
-    this.modalState = false;
-    this.URI = "";
-    this.photoExists = false;
-
-    //On Load Functions
     if (AppConstants.production){
       Mixpanel.trackLoad();
     }
 
+    this.list = [];
+    this.modalState = false;
+    this.URI = "";
+    this.photoExists = false;
   }
 
   cancelImage() {
@@ -44,6 +41,7 @@ class HomeController {
       console.err(err);
     });
   }
+
 
   upload() {
     this._Photos.uploadPhoto(imageURI, this._Location.getLocation()).then((res) =>
